cfe27c07a95d3292d669a54beb479be9f086165b
Jefino9488 <jefinojacob9488@gmail.com>
2025-06-26 19:39:27 +0530

    Refactor next.config.mjs to streamline configuration and remove unused environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
-    NEXT_PUBLIC_MONGODB_DB: process.env.NEXT_PUBLIC_MONGODB_DB,
-    NEXT_PUBLIC_GOOGLE_AI_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_AI_API_KEY,
-    NEXT_PUBLIC_BACKEND_BASE_URL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
-  },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: `${process.env.BACKEND_BASE_URL}/api/:path*`,
-  //     },
-  //   ]
-  // },
-  poweredByHeader: false,
-  generateEtags: false,
+    output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    poweredByHeader: false,
+    generateEtags: false,
 }
 
-export default nextConfig
+export default nextConfig;
